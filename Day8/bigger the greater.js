31bf369185da7f81a24d55767ccc4c7a30b417b7
Surya Punj Sharma <57707982+suryasps24@users.noreply.github.com>
2021-10-04 19:12:56 +0530

    Create bigger the greater.js

---
+++ Day8/bigger the greater.js
@@ -0,0 +1,26 @@
+ let array = w.split('');
+    // Find non-increasing suffix
+    var i = array.length - 1;
+    while (i > 0 && array[i - 1] >= array[i])
+        i--;
+    if (i <= 0)
+        return "no answer";
+    
+    // Find successor to pivot
+    var j = array.length - 1;
+    while (array[j] <= array[i - 1])
+        j--;
+    var temp = array[i - 1];
+    array[i - 1] = array[j];
+    array[j] = temp;
+    
+    // Reverse suffix
+    j = array.length - 1;
+    while (i < j) {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        i++;
+        j--;
+    }
+    return array.join('');
